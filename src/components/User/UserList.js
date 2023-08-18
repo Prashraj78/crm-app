876612e78d06e190f1dd53a2ea1c2114d87535aa
Prashant Patil <prashantrajputaaaa@gmail.com>
2023-08-18 12:05:50 +0530

    add user

--- src/components/User/UserList.js
+++ src/components/User/UserList.js
@@ -2,55 +2,59 @@ import { useEffect, useState } from "react";
 import NavBar from "../Navbar/Navbar";
 
 
-function UserList(){
-    // state is storage which when changes, 
-    // refreshes component.
-    const [users, setUsers]=useState([]);
+function UserList() {
+  // state is storage which when changes, 
+  // refreshes component.
+  const [users, setUsers] = useState([]);
 
-    useEffect(()=>{
-        // call api to get data.
-        fetch("http://localhost:4000/api/user")
-            .then(
-                // convert data to json format.
-                res => res.json()
-            ).then((parsedResult)=>{
-                // update state with data.
-                setUsers(parsedResult);
-            })
-    });
+  useEffect(() => {
+    // call api to get data.
+    fetch("http://localhost:4000/api/user")
+      .then(
+        // convert data to json format.
+        res => res.json()
+      ).then((parsedResult) => {
+        // update state with data.
+        setUsers(parsedResult);
+      })
+  }, []);
 
-    return(
-        <div>
-            <NavBar />
+  return (
+    <div>
+      <NavBar />
 
-            <div className="container">
-            <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Name</th>
-      <th scope="col">Email</th>
-      <th scope="col">Username</th>
-      <th scope="col">IsActive</th>
-    </tr>
-  </thead>
-  <tbody>
-    {
-        users.map(u=>
+      <div className="container">
+        <a href="/userForm" className="btn btn-success">
+          New User
+        </a>
+
+        <table class="table">
+          <thead>
             <tr>
-            <td>{u.name}</td>
-            <td>{u.email}</td>
-            <td>{u.username}</td>
-            <td>{u.isActive ? 'Yes' : 'No'}</td>
-          </tr>
-            
-            )
-    }
-   
-  </tbody>
-</table>
-            </div>
-        </div>
-    );
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Username</th>
+              <th scope="col">IsActive</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              users.map(u =>
+                <tr>
+                  <td scope="row">{u.name}</td>
+                  <td>{u.email}</td>
+                  <td>{u.username}</td>
+                  <td>{u.isActive ? 'Yes' : 'No'}</td>
+                </tr>
+
+              )
+            }
+
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default UserList;
